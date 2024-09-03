b14eb5097d99d1c98bb5b159a64736fc13f117c3
Papun <papun.1424@gmail.com>
2024-09-04 00:21:00 +0530

    kafka poc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kafka-poc
 This project talks about the basic of kafka poc
+
+https://gist.github.com/piyushgarg-dev/32cadf6420c452b66a9a6d977ade0b01
---
+++ producer.js
@@ -0,0 +1,37 @@
+const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function init() {
+  const producer = kafka.producer();
+
+  console.log("Connecting Producer");
+  await producer.connect();
+  console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location?.toString()?.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    console.log("Producer disconnected Successfully");
+    await producer.disconnect();
+  });
+}
+
+init();
